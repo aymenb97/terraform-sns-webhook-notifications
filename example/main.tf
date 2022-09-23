9fafa772361d8480134e4ba01997a56ac817479e
aymen <ayman.bellakhder@pi-square.io>
2022-09-23 10:31:32 +0100

    webhook endpoint fix

--- example/main.tf
+++ example/main.tf
@@ -3,5 +3,5 @@ module "sns_notify" {
   emails         = ["ayman.bellakhder@pi-square.io"]
   policy_arns    = ["arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"]
   function_name  = "sns-notify"
-  slack_endpoint = "https://hooks.slack.com/services/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"
+  slack_endpoint = "YOUR_SLACK_WEB_HOOK"
 }
